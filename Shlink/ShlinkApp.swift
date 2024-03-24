3f81e158475fe455651747bc14ec922baf561dd5
Wisse <wissehes@gmail.com>
2024-03-24 18:52:57 +0100

    Initial Commit

---
+++ Shlink/ShlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShlinkApp.swift
+//  Shlink
+//
+//  Created by Wisse Hes on 24/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
