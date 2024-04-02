ba7c198f6f49a0334d3fdb72a8ccbe52bdfd2877
Wisse <wissehes@gmail.com>
2024-04-02 23:00:46 +0200

    Create shlink class

---
+++ Shlink/Shlink.swift
@@ -0,0 +1,78 @@
+//
+//  Shlink.swift
+//  Shlink
+//
+//  Created by Wisse Hes on 24/03/2024.
+//
+
+import Foundation
+import Alamofire
+
+final class Shlink {
+    
+    static let apiKey = ""
+    static let baseUrl = ""
+    
+    static let shared = Shlink()
+    
+    let client: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.headers.add(.accept("application/json"))
+        configuration.headers.add(name: "X-Api-Key", value: Shlink.apiKey)
+        return Session(configuration: configuration)
+    }()
+    
+    func getShortUrls() async throws -> [ShortURL] {
+        let data = try await client.request(Self.baseUrl + "/short-urls")
+            .validate()
+            .serializingDecodable(ShortUrlsResponse.self)
+            .value
+        
+        return data.shortUrls.data
+    }
+    
+    func addShortUrl() async throws {
+        
+    }
+    
+}
+
+
+extension Shlink {
+    
+    struct ShortUrlsResponse: Codable {
+        let shortUrls: ShortUrls
+    }
+    
+    struct ShortUrls: Codable {
+        let data: [ShortURL]
+        let pagination: Pagination
+    }
+    
+    // MARK: - ShortURL
+    struct ShortURL: Codable {
+        let shortCode: String
+        let shortURL: String
+        let longURL: String
+        let dateCreated: String
+        let visitsCount: Int
+        let tags: [String]
+//        let meta: Meta
+//        let domain: JSONNull?
+        let title: String
+        let crawlable, forwardQuery: Bool
+
+        enum CodingKeys: String, CodingKey {
+            case shortCode
+            case shortURL = "shortUrl"
+            case longURL = "longUrl"
+            case dateCreated, visitsCount, tags, title, crawlable, forwardQuery
+        }
+    }
+    
+    // MARK: - Pagination
+    struct Pagination: Codable {
+        let currentPage, pagesCount, itemsPerPage, itemsInCurrentPage: Int
+        let totalItems: Int
+    }
+}
