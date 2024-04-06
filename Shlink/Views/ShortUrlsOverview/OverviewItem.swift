42f48b36e0c5634bdaf0a8ed69eecd66876b57fe
Wisse <wissehes@gmail.com>
2024-04-06 21:17:44 +0200

    Create ShortUrlsOverview folder

---
+++ Shlink/Views/ShortUrlsOverview/OverviewItem.swift
@@ -0,0 +1,18 @@
+//
+//  OverviewItem.swift
+//  Shlink
+//
+//  Created by Wisse Hes on 06/04/2024.
+//
+
+import SwiftUI
+
+struct OverviewItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverviewItem()
+}
