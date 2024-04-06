e1f85074be291e3149b0fb874e4403fbd202ce03
Wisse <wissehes@gmail.com>
2024-04-06 18:28:32 +0200

    Move ContentView code to HomeView

--- Shlink/ContentView.swift
+++ Shlink/ContentView.swift
@@ -9,28 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var vm = ShortUrlsViewModel()
-    
-    @State private var isShowingAddSheet = false
-    
     var body: some View {
         NavigationStack {
-            ShortUrlsOverview(vm: vm)
-                .navigationTitle("Short URLs")
-                .toolbar { self.toolbar }
-                .sheet(isPresented: $isShowingAddSheet) {
-                    CreateShortUrlView(vm: vm)
-                }
         }
     }
     
-    var toolbar: some ToolbarContent {
-        ToolbarItem(placement: .primaryAction) {
-            Button("Add short URL", systemImage: "plus.circle") {
-                isShowingAddSheet.toggle()
-            }
-        }
-    }
 }
 
 #Preview {
---
+++ Shlink/Views/HomeView.swift
@@ -0,0 +1,47 @@
+//
+//  HomeView.swift
+//  Shlink
+//
+//  Created by Wisse Hes on 06/04/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var server: Server
+    
+    init(server: Server) {
+        self._vm = .init(initialValue: .init(server: server))
+        self.server = server
+    }
+    
+    @State private var vm: ShortUrlsViewModel
+    @State private var isShowingAddSheet = false
+    
+    var body: some View {
+        NavigationStack {
+            ShortUrlsOverview(vm: vm)
+                .navigationTitle("Short URLs")
+                .toolbar { self.toolbar }
+                .sheet(isPresented: $isShowingAddSheet) {
+                    CreateShortUrlView(vm: vm)
+                }
+        }
+    }
+    
+    var toolbar: some ToolbarContent {
+        ToolbarItem(placement: .primaryAction) {
+            Button("Add short URL", systemImage: "plus.circle") {
+                isShowingAddSheet.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    let container = DataController.previewContainer
+    let server = Server.previewServer()
+    
+    return HomeView(server: server)
+        .modelContainer(container)
+}
